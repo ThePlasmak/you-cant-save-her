c8be4227abf67cbca58644ec2d913526e4d9b92f
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-02 22:48:30 +0800

    Reload, new undo system

--- src/javascript.js
+++ src/javascript.js
@@ -2,12 +2,40 @@
 UIBar.destroy();
 
 // UNDO BUTTON
-function updateUndoButton() {
-  // Remove existing undo button if it exists
-  $("#undobar").remove();
+// Simplify the initial check and setting of the enableUndo variable
+$(document).on(":start", function () {
+  // Correctly set State variable based on localStorage, with a fallback to true if not set
+  State.variables.enableUndo =
+    localStorage.getItem("enableUndo") !== null
+      ? localStorage.getItem("enableUndo") === "true"
+      : true;
+});
 
-  // Add the undo button if $enable_undo is true
-  if (State.variables.enable_undo) {
+setup.enableUndo = {
+  // Retrieves the current undo state from localStorage, correctly handling "true" and "false" strings
+  get: function () {
+    var value = localStorage.getItem("enableUndo");
+    return value === "true"; // Correctly returns false if value is "false"
+  },
+
+  // Toggles the undo state and saves the new state to localStorage
+  toggle: function () {
+    var currentState = this.get();
+    localStorage.setItem("enableUndo", !currentState);
+
+    // Ensure the state variable is also updated
+    State.variables.enableUndo = !currentState;
+
+    // Optionally, refresh the current passage to reflect the change immediately
+    Engine.play(passage());
+  },
+};
+
+function updateUndoButton() {
+  $("#undobar").remove(); // Remove existing Undo button, if any.
+
+  if (setup.enableUndo.get()) {
+    // Use the setup object's method for consistency.
     $("body").prepend(
       "<div id='undobar'><a href='#' id='undoButton'>←</a></div>"
     );
@@ -19,14 +47,13 @@ function updateUndoButton() {
   }
 }
 
-// Run the update function on each passage render
-$(document).on(":passagerender", function () {
-  updateUndoButton();
-});
+// Initialize the undo button based on the current state
+$(document).on(":passagerender", updateUndoButton);
 
 // AUTOSAVE
 $(document).on(":passagerender", function () {
-  if (State.passage !== "Start") {
+  // Check if the current passage is neither "Start" nor "options"
+  if (State.passage !== "Start" && State.passage !== "options") {
     Save.slots.save(1); // Automatically save to slot 1
   }
 });
@@ -51,3 +78,19 @@ $(document).on(":passagerender", function (ev) {
     });
   }
 });
+
+// GO TO START ON REFRESH
+window.onbeforeunload = function () {
+  window.sessionStorage.setItem("twine-reload-flag", "true");
+};
+
+postdisplay["refresh-taker"] = function (t) {
+  delete postdisplay[t];
+  var refresh = sessionStorage.getItem("twine-reload-flag");
+
+  if (refresh === "true" && passage() !== "Start") {
+    // your starting passage here
+
+    Engine.play("Start"); // passage to forward player to
+  }
+};
