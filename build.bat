e51ec1bd796d34e621711155b83d7d377c42b5a4
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-04 02:39:45 +0800

    Prep for non-workshop versions

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-call tweego -l -o "export\advanced-creative-writing-workshop.html" src
-python scripts/loading_screen_remover.py "export\advanced-creative-writing-workshop.html"
-call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\advanced-creative-writing-workshop.html" src
-python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\advanced-creative-writing-workshop.html"
+call tweego -l -o "export\advanced-creative-writing.html" src
+python scripts/loading_screen_remover.py "export\advanced-creative-writing.html"
+call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced-Creative-Writing.html" src
+python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced-Creative-Writing.html"
 python scripts/twee_to_docx.py
