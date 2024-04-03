40b058f492a7627a750b1205c41391186051cb27
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-03 17:57:11 +0800

    Set up loading screen remover

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 call tweego -l -o "export\advanced-creative-writing-workshop.html" src
+python scripts/loading_screen_remover.py "export\advanced-creative-writing-workshop.html"
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\advanced-creative-writing-workshop.html" src
+python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\advanced-creative-writing-workshop.html"
 python scripts/twee_to_docx.py
---
+++ scripts/loading_screen_remover.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+loading_background_color = "#310001"
+
+
+def modify_html_file(file_path):
+    try:
+        # Open the file for reading
+        with open(file_path, "r", encoding="utf-8") as file:
+            file_contents = file.read()
+
+        # Replace the specified string
+        file_contents = file_contents.replace(
+            f"Helmet,Freesans,sans-serif;font-weight:700;color:#eee;background-color:#111;text-align:center",
+            f"Helmet,Freesans,sans-serif;font-weight:700;color:{loading_background_color};background-color:{loading_background_color};text-align:center",
+        )
+
+        # Remove the specified string
+        file_contents = re.sub(r"#init-loading\{[^\}]+\}", "", file_contents)
+
+        # Write the modified contents back to the file
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(file_contents)
+
+        print(f"The {file_path} has been modified successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_html_file>")
+    else:
+        file_path = sys.argv[1]
+        modify_html_file(file_path)
