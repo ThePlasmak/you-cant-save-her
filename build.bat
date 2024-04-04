68441aa0fe956e9a3cb2f88c1ce3d6e5cb86880e
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-05 00:07:14 +0800

    Neaten and add Poof

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
-call tweego -l -o "export\advanced-creative-writing.html" src
-python scripts/loading_screen_remover.py "export\advanced-creative-writing.html"
-call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced-Creative-Writing.html" src
-python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced-Creative-Writing.html"
+call tweego -l -o "export\Advanced_Creative_Writing.html" src
+python scripts/loading_screen_remover.py "export\Advanced_Creative_Writing.html"
+
+call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced_Creative_Writing.html" src
+python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced_Creative_Writing.html"
+
+python scripts/twee_to_poof.py
+
 python scripts/twee_to_docx.py
