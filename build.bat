bd15d9433673e9547d194c473a1b7ebde5103973
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-09 23:35:13 +0800

    Fix path to private

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 call tweego -l -o "export\Advanced_Creative_Writing.html" src
 python scripts/loading_screen_remover.py "export\Advanced_Creative_Writing.html"
 
-call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\Advanced_Creative_Writing.html" src
+call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\Advanced_Creative_Writing.html" src
 python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\Advanced_Creative_Writing.html"
 
 python scripts/twee_to_poof.py
