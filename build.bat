70d80d2bdf3b94cf7b9fc25dcd9883fd40343fcc
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-11 01:45:08 +0800

    Adjust for renames, and add testing folder

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
+call tweego -l -o "testing\Advanced_Creative_Writing.html" src
+python python_scripts/loading_screen_remover.py "testing\Advanced_Creative_Writing.html"
+
 call tweego -l -o "export\Advanced_Creative_Writing.html" src
-python scripts/loading_screen_remover.py "export\Advanced_Creative_Writing.html"
+python python_scripts/loading_screen_remover.py "export\Advanced_Creative_Writing.html"
 
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\Advanced_Creative_Writing.html" src
-python scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\Advanced_Creative_Writing.html"
+python python_scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\Advanced_Creative_Writing.html"
 
-python scripts/twee_to_poof.py
+python python_scripts/twee_to_poof.py
 
-python scripts/twee_to_docx.py
+python python_scripts/twee_to_docx.py
