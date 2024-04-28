17d1c2e8d88c44aa6ffe43b5079a46e131cf4767
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-28 23:07:29 +0800

    Comment out parts I do not need

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ python python_scripts/loading_screen_remover.py "export\You_Can't_Save_Her.html"
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html" src
 python python_scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html"
 
-python python_scripts/twee_to_poof.py
+@REM python python_scripts/twee_to_poof.py
 
-python python_scripts/twee_to_docx.py
+@REM python python_scripts/twee_to_docx.py
