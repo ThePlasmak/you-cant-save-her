e031e34ae111c4bcdccf7af3b914b39236cf4855
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-04-29 16:38:41 +0800

    Hamburger attempt

--- src/javascript.js
+++ src/javascript.js
@@ -64,6 +64,43 @@ function updateUndoButton() {
 // Initialize the undo button on passage render
 $(document).on(":passagerender", updateUndoButton);
 
+// HAMBURGER MENU
+function updateHamburgerButton() {
+  $("#hamburgerNav").remove(); // Remove existing Hamburger button, if any.
+
+  if (
+    State.passage !== "Start" &&
+    State.passage !== "options" &&
+    State.passage !== "content_warning" &&
+    State.passage !== "content_warning_restart" &&
+    State.passage !== "its_okay" &&
+    State.passage !== "credits" &&
+    State.passage !== "chapters" &&
+    State.passage !== "restart"
+  ) {
+    $("body").prepend(
+      `<nav role="navigation" id="hamburgerNav">
+          <div id="hamburgerToggle">
+            <input type="checkbox" />
+
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul id="hamburger">
+              <a href="#"><li>Home</li></a>
+              <a href="#"><li>About</li></a>
+              <a href="#"><li>Info</li></a>
+              <a href="#"><li>Contact</li></a>
+            </ul>
+          </div>
+        </nav>`
+    );
+  }
+}
+
+$(document).on(":passagerender", updateHamburgerButton);
+
 // AUTOSAVE
 // Autosaves whenever you enter a new passage
 $(document).on(":passagerender", function () {
@@ -89,9 +126,19 @@ $(document).on(":passagerender", function (ev) {
   // Get the current passage's tags
   var currentTags = tags();
 
-  // Check if tag is included
+  // Reset default styles first
+  $("#passages").css({
+    "background-color": "",
+    "box-shadow": "",
+    color: "",
+    padding: "",
+  });
+  $("#story").css({
+    margin: "",
+  });
+
+  // Check and apply styles based on tags
   if (currentTags.includes("flashback")) {
-    // Apply styles for passages with the tag
     $("#passages").css({
       "background-color": "transparent",
       "box-shadow": "none",
@@ -99,7 +146,7 @@ $(document).on(":passagerender", function (ev) {
     });
   } else if (currentTags.includes("options")) {
     $("#passages").css({
-      "background-color": "rgb(27 27 27)",
+      "background-color": "rgb(27, 27, 27)",
       "box-shadow": "var(--shadow-elevation-high)",
     });
   } else if (currentTags.includes("full-text-white")) {
@@ -107,43 +154,26 @@ $(document).on(":passagerender", function (ev) {
       padding: "0",
       color: "white",
     });
-  } else {
-    // Reset styles for passages without certain tags
-    $("#passages").css({
-      "background-color": "",
-      "box-shadow": "",
-      color: "",
-      padding: "",
-      transition: "",
+    $("#story").css({
+      margin: "0.5em",
     });
   }
 });
 
-// // Fix passages with the no-t8n tag having the wrong text color when undoed to
-// var isWhitePassage = ["teen_flashback_15"].includes(State.passage);
+// GO TO START ON REFRESH
+// window.onbeforeunload = function () {
+//   window.sessionStorage.setItem("twine-reload-flag", "true");
+// }; // Set the reload flag before the page unloads
 
-// $(document).on(":passagerender", function () {
-//   if (State.passage == "teen_flashback_15") {
-//     $("#passages").css("color", "white");
-//   } else {
-//     $("#passages").css("color", ""); // Reset to default or another specified value if needed
+// // Use the :passagedisplay event to check the flag and redirect if necessary
+// $(document).on(":passagedisplay", function () {
+//   var refresh = sessionStorage.getItem("twine-reload-flag");
+
+//   // Clear the flag immediately after retrieving it to prevent unintended behavior
+//   sessionStorage.removeItem("twine-reload-flag");
+
+//   if (refresh === "true" && passage() !== "Start") {
+//     // Ensure to only redirect if not on the 'Start' passage to avoid loops
+//     Engine.play("Start"); // Replace 'somePassage' with your target passage
 //   }
 // });
-
-// GO TO START ON REFRESH
-window.onbeforeunload = function () {
-  window.sessionStorage.setItem("twine-reload-flag", "true");
-}; // Set the reload flag before the page unloads
-
-// Use the :passagedisplay event to check the flag and redirect if necessary
-$(document).on(":passagedisplay", function () {
-  var refresh = sessionStorage.getItem("twine-reload-flag");
-
-  // Clear the flag immediately after retrieving it to prevent unintended behavior
-  sessionStorage.removeItem("twine-reload-flag");
-
-  if (refresh === "true" && passage() !== "Start") {
-    // Ensure to only redirect if not on the 'Start' passage to avoid loops
-    Engine.play("Start"); // Replace 'somePassage' with your target passage
-  }
-});
