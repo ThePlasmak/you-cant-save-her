f4958a135baa6a1bb13097046658cb37bfe5a30e
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-07-02 01:02:22 +0800

    Comment out instead

--- build.bat
+++ build.bat
@@ -4,6 +4,9 @@ python python_scripts/loading_screen_remover.py "export\You_Can't_Save_Her.html"
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html" src
 python python_scripts/loading_screen_remover.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html"
 
+@REM call tweego -l -o "testing\You_Can't_Save_Her.html" src
+@REM python python_scripts/loading_screen_remover.py "testing\You_Can't_Save_Her.html"
+
 @REM python python_scripts/twee_to_poof.py
 
 @REM python python_scripts/twee_to_docx.py
