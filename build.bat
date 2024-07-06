8244fbf6fbb0e885a293ade2c72922db0b11e5c6
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-07-07 02:30:45 +0800

    Rename to index due to Itch.io

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
-call tweego -l -o "export\You_Can't_Save_Her.html" src
-python python_scripts/html_editor.py "export\You_Can't_Save_Her.html"
+call tweego -l -o "export\index.html" src
+python python_scripts/html_editor.py "export\index.html"
 
 call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html" src
 python python_scripts/html_editor.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html"
 
-call tweego -l -o "testing\You_Can't_Save_Her.html" src
-python python_scripts/html_editor.py "testing\You_Can't_Save_Her.html"
+call tweego -l -o "testing\index.html" src
+python python_scripts/html_editor.py "testing\index.html"
 
 @REM python python_scripts/twee_to_poof.py
 
