0d2fa0f1639d155253decfb48e13e77fca4d0459
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-07-25 16:58:36 +0800

    Add butler to build command

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
-call tweego -l -o "export\index.html" src
-python python_scripts/html_editor.py "export\index.html"
+@REM call tweego -l -o "export\index.html" src
+@REM python python_scripts/html_editor.py "export\index.html"
 
-call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html" src
-python python_scripts/html_editor.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html"
+@REM call tweego -l -o "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html" src
+@REM python python_scripts/html_editor.py "C:\Users\Sarah\Documents\GitHub\theplasmak.github.io\private\You_Can't_Save_Her.html"
 
 call tweego -l -o "testing\index.html" src
 python python_scripts/html_editor.py "testing\index.html"
+call butler push testing sarahmak/you-cant-save-her:win-linux-mac-android
 
 @REM python python_scripts/twee_to_poof.py
 
