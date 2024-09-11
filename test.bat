da7d44800f9c1f3ecca95b8de4e3fd1e48f9b532
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-09-11 11:53:04 +0800

    Make testing folder if it does not exist

--- test.bat
+++ test.bat
@@ -1,2 +1,5 @@
+@REM Create the "testing" folder if it doesn't exist
+if not exist "testing" mkdir testing
+
 @REM Constantly recompiles when it detects changes in src
 call tweego -l -o "testing\index.html" src --watch
