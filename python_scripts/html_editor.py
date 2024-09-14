6dc7f862d25ea4f35597a00a2c5aa352ab013c92
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-09-14 23:26:35 +0800

    Fix favicon not appearing

--- python_scripts/html_editor.py
+++ python_scripts/html_editor.py
@@ -35,10 +35,10 @@ def modify_html_file(file_path):
 <meta property="og:description" content="An interactive fiction game about fighting your former friend." />
 <meta property="twitter:description" content="An interactive fiction game about fighting your former friend." />
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
 <meta name="msapplication-TileColor" content="#2b5797">
 <meta name="theme-color" content="#ffffff">""",
         )
