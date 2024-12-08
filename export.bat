ea8935eb11f25b7072ecf86c6125187d64518c8c
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-12-09 00:08:31 +0800

    Change username to plasmak

--- export.bat
+++ export.bat
@@ -6,7 +6,7 @@
 
 call tweego -l -o "testing\index.html" src
 python python_scripts/html_editor.py "testing\index.html"
-call butler push testing sarahmak/you-cant-save-her:win-linux-mac-android
+call butler push testing plasmak/you-cant-save-her:win-linux-mac-android
 powershell -Command "New-Item -ItemType Directory -Force -Path 'temp_dir'; Copy-Item 'testing\*' 'temp_dir' -Recurse; Compress-Archive -Path 'temp_dir\*' -DestinationPath 'export/you-cant-save-her.zip' -Force; Remove-Item 'temp_dir' -Recurse"
 
 @REM python python_scripts/twee_to_poof.py
